bee7d63b61dbad85d88a25418836319183454455
SolSurge <49626818+SolSurge@users.noreply.github.com>
2020-04-28 21:18:47 -0500

    Adjusted Spawn Rates

--- commands/fishing/fish.js
+++ commands/fishing/fish.js
@@ -18,10 +18,10 @@ module.exports = {
         try {
             if (amount && amount <= 10) {
                 for (let i = 0; i < amount; i++) {
-                    var Rarity = randomNumber(1, 201);
+                    var Rarity = randomNumber(1, 100);
 
-                    // Common Fish Loop with a range of 1-100
-                    if (Rarity > 0 && Rarity < 101) {
+                    // Common Fish Loop with a range of 1-40 (40%)
+                    if (Rarity > 0 && Rarity < 41) {
                         var CommonID = randomNumber(1, 20);
 
                         for (let j = 1; j < 20; ++j) {
@@ -37,8 +37,8 @@ module.exports = {
                         }
                     }
 
-                    // Uncommon Fish Loop with a range of 101-150
-                    if (Rarity > 100 && Rarity < 151) {
+                    // Uncommon Fish Loop with a range of 41-63 (23%)
+                    if (Rarity > 40 && Rarity < 64) {
                         var UncommonID = randomNumber(21, 20);
 
                         for (let j = 21; j < 40; ++j) {
@@ -54,8 +54,8 @@ module.exports = {
                         }
                     }
 
-                    // Rare Fish Loop with a range of 151-185
-                    if (Rarity > 150 && Rarity < 186) {
+                    // Rare Fish Loop with a range of 64-76 (13%)
+                    if (Rarity > 63 && Rarity < 77) {
                         var RareID = randomNumber(41, 20);
 
                         for (let j = 41; j < 60; ++j) {
@@ -71,8 +71,8 @@ module.exports = {
                         }
                     }
 
-                    // Unique Fish Loop with a range of 186-193
-                    if (Rarity > 185 && Rarity < 194) {
+                    // Unique Fish Loop with a range of 77-86 (10%)
+                    if (Rarity > 76 && Rarity < 87) {
                         var UniqueID = randomNumber(61, 20);
 
                         for (let j = 61; j < 80; ++j) {
@@ -88,8 +88,8 @@ module.exports = {
                         }
                     }
 
-                    // Epic Fish Loop with a range of 194-197
-                    if (Rarity > 193 && Rarity < 198) {
+                    // Epic Fish Loop with a range of 87-93 (7%)
+                    if (Rarity > 86 && Rarity < 94) {
                         var EpicID = randomNumber(81, 20);
 
                         for (let j = 81; j < 110; ++j) {
@@ -105,8 +105,8 @@ module.exports = {
                         }
                     }
 
-                    // Wicked Fish Loop with a range of 198-199
-                    if (Rarity > 197 && Rarity < 200) {
+                    // Wicked Fish Loop with a range of 94-97 (4%)
+                    if (Rarity > 93 && Rarity < 96) {
                         var WickedID = randomNumber(101, 20);
 
                         for (let j = 101; j < 120; ++j) {
@@ -122,8 +122,8 @@ module.exports = {
                         }
                     }
 
-                    // Legendary Fish Loop with a range of 200
-                    if (Rarity == 200) {
+                    // Legendary Fish Loop with a range of 98-99 (2%)
+                    if (Rarity > 97 && Rarity < 100) {
                         var LegendaryID = randomNumber(121, 20);
 
                         for (let j = 121; j < 140; ++j) {
@@ -139,8 +139,8 @@ module.exports = {
                         }
                     }
 
-                    // Mythical Fish Loop with a range of 201
-                    if (Rarity == 201) {
+                    // Mythical Fish Loop with a range of 100 (1%)
+                    if (Rarity == 100) {
                         var MythicalID = randomNumber(141, 20);
 
                         for (let j = 141; j < 160; ++j) {
