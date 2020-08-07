510eeb91bbc4ee06bd8a3701f12c8b4f78fb4993
SolSurge <49626818+SolSurge@users.noreply.github.com>
2020-08-07 15:27:09 -0500

    Create kawaii.js
    Added KawaiiBot functions

---
+++ commands/general/kawaii.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'kawaii',
+    aliases: ['KawaiiBot'],
+    category: 'General',
+    description: 'Handles general functions of old KawaiiBot',
+    run: async (bot, message, args) =>
+    {  // Sets max range of random roll to 10
+       if (args[0] && args[0] === '+roll10')
+       {
+          var roll = randomNumber(1, 10);
+          return message.channel.send(`Roll: ${roll}`);
+       } else return message.channel.send("Sorry can you type that again!");
+       // Sets max range of random roll to 20
+       if (args[0] && args[0] === '+roll20')
+       {
+          var roll = randomNumber(1, 20);
+          return message.channel.send(`Roll: ${roll}`);
+       } else return message.channel.send("Sorry can you type that again!");
+       // Chip pats user for affection
+       if (args[0] && args[0] === '+pat')
+       {
+          return message.channel.send(`Chip gently pats your head!`);
+       } else return message.channel.send("Sorry can you type that again!");
+       // Chip hugs user for affection
+       if (args[0] && args[0] === '+hug')
+       {
+          return message.channel.send(`Chip gives you a giant bear hug!`);
+       } else return message.channel.send("Sorry can you type that again!");
+       // Chip kisses user for affection
+       if (args[0] && args[0] === '+kiss')
+       {
+          return message.channel.send(`Chip kisses you on the cheeck!`);         
+       } else return message.channel.send("Sorry can you type that again!");
+    }
+}
